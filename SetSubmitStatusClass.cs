4828104b9d2f8c01ca14a26b781750551a765592
rvsure <60949645+rvsure@users.noreply.github.com>
2020-02-12 09:10:38 +0800

    Added comments

--- SetSubmitStatusClass.cs
+++ SetSubmitStatusClass.cs
@@ -7,9 +7,11 @@ namespace SCMadeEasy.Feature.ExperienceFormsExtensions.Pipelines
     {
         public override void Process(RenderFormEventArgs args)
         {
+            //Check if the request post request to make sure the code executes only on form submission
             if (args.IsPost)
             {
                 string responseClass = string.Empty;
+                //Check if the model state is valid to confirm successful form submission
                 if (args.HtmlHelper.ViewData.ModelState.IsValid)
                 {
                     responseClass = "submit-success";
@@ -18,6 +20,7 @@ namespace SCMadeEasy.Feature.ExperienceFormsExtensions.Pipelines
                 {
                     responseClass = "submit-error";
                 }
+                //Make sure we are not overwriting the existing CSS classes
                 args.ViewModel.CssClass += string.IsNullOrWhiteSpace(args.ViewModel.CssClass) ? responseClass : " " + responseClass;
             }
         }
