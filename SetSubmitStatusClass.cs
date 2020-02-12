c9b55f7ce1d5c5a1d1fae8764221b768dc1bb500
rvsure <60949645+rvsure@users.noreply.github.com>
2020-02-12 09:08:11 +0800

    Added the pipeline processor and config files

---
+++ SetSubmitStatusClass.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.ExperienceForms.Mvc.Pipelines.RenderForm;
+using Sitecore.Mvc.Pipelines;
+
+namespace SCMadeEasy.Feature.ExperienceFormsExtensions.Pipelines
+{
+    public class SetSubmitStatusClass : MvcPipelineProcessor<RenderFormEventArgs>
+    {
+        public override void Process(RenderFormEventArgs args)
+        {
+            if (args.IsPost)
+            {
+                string responseClass = string.Empty;
+                if (args.HtmlHelper.ViewData.ModelState.IsValid)
+                {
+                    responseClass = "submit-success";
+                }
+                else
+                {
+                    responseClass = "submit-error";
+                }
+                args.ViewModel.CssClass += string.IsNullOrWhiteSpace(args.ViewModel.CssClass) ? responseClass : " " + responseClass;
+            }
+        }
+    }
+}
